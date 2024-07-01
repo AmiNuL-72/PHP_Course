8f3f407d06398cf333bad41501d71d2eac24adab
AmiNuL-72 <157211474+AmiNuL-72@users.noreply.github.com>
2024-07-01 20:57:36 +0600

    Add files via upload

---
+++ 0LL-PHP_Project/Project01:"Dynamic_StudentID_Card"/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StudentID</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="Identity_Card_Design.php" method="POST">
+        
+        <h1>Create Your Virtual ID Card</h1>
+        <label for="name">Name : </label>
+        <input type="text" name="name" placeholder="Your Full name"> <br>
+
+        <label for="phn">Phone : </label>
+        <input type="phone" name="phn" placeholder="+880-XXXXXXXXXX"> <br>
+
+        <label for="email">Email : </label>
+        <input type="email" name="email" placeholder="abc@gmail.com"> <br>
+
+        <label for="crs">Course : </label>
+        <input type="text" name="crs" placeholder="Name of Your Course"> <br>
+
+        <label for="roll">Roll No : </label>
+        <input type="text" name="roll" placeholder="Your roll No."> <br>
+
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
